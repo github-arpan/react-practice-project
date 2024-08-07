3d0d57192db9e7d4aa549a6732244b1513fe78d1
github-arpan <develop.arpan@gmail.com>
2024-08-07 23:33:02 +0530

    github profile finder

--- githubProfileFinder/src/Card.jsx
+++ githubProfileFinder/src/Card.jsx
@@ -5,13 +5,13 @@ function Card({ userData }) {
   return userData !== null ? (
     <div className="flex justify-center items-center mt-10">
       <div>
-        <div>
+        <div className="flex justify-center">
           <img src={avatar_url} alt="user" />
         </div>
-        <div className="mt-4 space-x-2  ">
+        <div className="mt-4 space-x-2 max-w-[70%] mx-auto ">
           <h3 className="font-bold">Name : {name || login}</h3>
           <p>Bio : {bio}</p>
-          <p> Following : {following} </p>
+          <p>Following : {following} </p>
           <p>Followers : {followers}</p>
         </div>
       </div>
